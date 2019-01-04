43e14e47171af4edfadb3932006c926d3b2c87a0
severia <anja.severin@daimler.com>
2019-01-04 18:10:23 +0100

    initial implementation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -163,6 +164,12 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+//calculate lane id from d value
+int lane_id_from_d(double d)
+{
+    return (int)d%4;
+}
+
 int main() {
   uWS::Hub h;
 
@@ -235,15 +242,173 @@ int main() {
           	double end_path_d = j[1]["end_path_d"];
 
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
+            // returns [id, x, y, vx, vy, s, d], has type vector<vector<double>>
           	auto sensor_fusion = j[1]["sensor_fusion"];
+          	int prev_size = previous_path_x.size();
+            // start in lane 1
+            int lane = 1;
+            //reference velocity to target
+            double ref_vel = 0; //mph
 
-          	json msgJson;
+            if (prev_size>0)
+            {
+                car_s = end_path_s;
+            }
+            bool too_close = false;
 
-          	vector<double> next_x_vals;
+            //find ref_v to use
+            for(int i = 0; i<sensor_fusion.size();i++)
+            {
+                float d =sensor_fusion[i][6];
+                if (d< (2+4*lane+2) && d> (2+4*lane -2))
+                {
+                    // this car is in ego vehicle lane
+                    double vx = sensor_fusion[i][3];
+                    double vy = sensor_fusion[i][4];
+                    double check_speed = sqrt(vx*vx+vy*vy);
+                    double check_car_s = sensor_fusion[i][5];
+
+                    check_car_s += ((double)prev_size*0.02*check_speed);
+                    //check s value creater than mine and s gap
+                    if((check_car_s >car_s) && ((check_car_s-check_car_s)<30))
+                    {
+                        // do some logic here, lower reference velocity so we dont
+                        //crash into other car in front of use, or flag to try to change lane
+                        too_close = true;
+                        if(lane >0)
+                        {
+                            lane = 0;
+                        }
+                    }
+                }
+            }
+
+            if(too_close){
+                ref_vel -= .224;
+            }
+            else if (ref_vel < 49.5)
+            {
+                ref_vel += .224;
+            }
+
+            // Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
+            // later we will interpolate these waypoints with a spline and fill it in with more points that control speed
+
+            vector <double> ptsx;
+            vector <double> ptsy;
+
+            //reference x, y, yaw states
+            // either we will reference the starting point as where the car is or at the previous path and point
+            double ref_x = car_x;
+            double ref_y = car_y;
+            double ref_yaw = deg2rad(car_yaw);
+
+            //if previous size is almost empty, use the car as starting reference
+            if (prev_size<2) {
+                //use two points that make the path tangent to the car
+                double prev_car_x = car_x - cos(car_yaw);
+                double prev_car_y = car_y - sin(car_yaw);
+
+                ptsx.push_back(prev_car_x);
+                ptsy.push_back(prev_car_y);
+
+                ptsx.push_back(car_x);
+                ptsy.push_back(car_y);
+            }
+            //use the previous path's end point as starting reference
+            {
+                //redefine reference state as previous path end point
+                ref_x = previous_path_x[prev_size-1];
+                ref_y = previous_path_y[prev_size-1];
+                double ref_x_prev = previous_path_x[prev_size-2];
+                double ref_y_prev = previous_path_y[prev_size-2];
+                ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+                //use two points that make the path tangent to the previous paths end point
+                ptsx.push_back(ref_x_prev);
+                ptsx.push_back(ref_x);
+                ptsy.push_back(ref_y_prev);
+                ptsy.push_back(ref_y);
+            }
+
+            // In Frenet add evenly 30m spaced points ahead of the starting reference
+            vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for (int i = 0;i < ptsx.size(); i++){
+                //shift car reference angle to 0 degree
+                double shift_x = ptsx[i] - ref_x;
+                double shift_y = ptsy[i] - ref_y;
+
+                ptsx[i] = (shift_x*cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+                ptsy[i] = (shift_x*sin(0-ref_yaw)-shift_y*cos(0-ref_yaw));
+            }
+
+            // create a spline
+            tk::spline s;
+
+            // set (x,y) points to the spline
+            s.set_points(ptsx,ptsy);
+
+            // define the actual (x,y) points we will us efor the planner
+            vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+          	// start with all of the previous path points from last time
+          	for(int i = 0; i<prev_size;i++)
+            {
+          	    next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+            }
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+          	// calculate how to break up spline points so that we travel at desired ference velocity
+          	double target_x = 30.0;
+          	double target_y = s(target_x);
+          	double target_dist = sqrt((target_x)*(target_x) + target_y*target_y);
+          	double x_add_on = 0;
+
+          	// Fill up the rest of our path planner after filling it with previous points
+          	// here we will always output 50 points
+          	for(int i = 1; i<= 50-prev_size; i++)
+            {
+          	    double N = (target_dist/(.02*ref_vel/2.24));  //converting from miles per into meter per second
+          	    double x_point = x_add_on + (target_x)/N;
+          	    double y_point = s(x_point);
+          	    x_add_on = x_point;
+
+          	    double x_ref = x_point;
+          	    double y_ref = y_point;
+
+          	    // rotate back to normal after rotating it earlier
+          	    x_point = (x_ref * cos(ref_yaw)-y_ref*sin(ref_yaw));
+          	    y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+          	    x_point += ref_x;
+          	    y_point += ref_y;
+
+          	    next_x_vals.push_back(x_point);
+          	    next_y_vals.push_back(y_points);
+            }
+
+            if (car_s == 0)
+            {
+                Vehicle ego_vehicle = Vehicle::Vehicle(lane_id_from_d(car_d), car_s, car_speed, 0, "CS");
+            }
+
+            // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+            // TODO: calculate next s and d
+            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+            json msgJson;
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
---
+++ src/vehicle.cpp
@@ -0,0 +1,131 @@
+#include <algorithm>
+#include <iostream>
+#include "vehicle.h"
+#include <cmath>
+#include <map>
+#include <string>
+#include <iterator>
+#include "cost.h"
+
+/**
+ * Initializes Vehicle
+ */
+
+Vehicle::Vehicle(){}
+
+Vehicle::Vehicle(int lane, float s, float v, float a, string state) {
+
+    this->lane = lane;
+    this->s = s;
+    this->v = v;
+    this->a = a;
+    this->state = state;
+    max_acceleration = -1;
+
+}
+
+Vehicle::~Vehicle() {}
+
+vector<Vehicle> Vehicle::choose_next_state(map<int, vector<Vehicle>> predictions) {
+    /*
+    INPUT: A predictions map. This is a map of vehicle id keys with predicted
+        vehicle trajectories as values. Trajectories are a vector of Vehicle objects representing
+        the vehicle at the current timestep and one timestep in the future.
+    OUTPUT: The the best (lowest cost) trajectory corresponding to the next ego vehicle state.
+
+    */
+    vector<string> states = successor_states();
+    float cost;
+    vector<float> costs;
+    vector<vector<Vehicle>> final_trajectories;
+
+    for (vector<string>::iterator it = states.begin(); it != states.end(); ++it) {
+        vector<Vehicle> trajectory = generate_trajectory(*it, predictions);
+        if (trajectory.size() != 0) {
+            cost = calculate_cost(*this, predictions, trajectory);
+            costs.push_back(cost);
+            final_trajectories.push_back(trajectory);
+        }
+    }
+
+    vector<float>::iterator best_cost = min_element(begin(costs), end(costs));
+    int best_idx = distance(begin(costs), best_cost);
+    return final_trajectories[best_idx];
+}
+
+vector<string> Vehicle::successor_states() {
+    /*
+    Provides the possible next states given the current state for the FSM
+    discussed in the course, with the exception that lane changes happen
+    instantaneously, so LCL and LCR can only transition back to KL.
+    */
+
+    vector<string> states;
+    states.push_back("KL");
+    string state = this->state;
+    if(state.compare("KL") == 0) {
+        states.push_back("PLCL");
+        states.push_back("PLCR");
+    } else if (state.compare("PLCL") == 0) {
+        if (lane != lanes_available - 1) {
+            states.push_back("PLCL");
+            states.push_back("LCL");
+        }
+    } else if (state.compare("PLCR") == 0) {
+        if (lane != 0) {
+            states.push_back("PLCR");
+            states.push_back("LCR");
+        }
+    }
+    //If state is "LCL" or "LCR", then just return "KL"
+    return states;
+}
+
+vector<Vehicle> Vehicle::generate_trajectory(string state, map<int, vector<Vehicle>> predictions) {
+    /*
+    Given a possible next state, generate the appropriate trajectory to realize the next state.
+    */
+    vector<Vehicle> trajectory;
+    if (state.compare("CS") == 0) {
+        trajectory = constant_speed_trajectory();
+    } else if (state.compare("KL") == 0) {
+        trajectory = keep_lane_trajectory(predictions);
+    } else if (state.compare("LCL") == 0 || state.compare("LCR") == 0) {
+        trajectory = lane_change_trajectory(state, predictions);
+    } else if (state.compare("PLCL") == 0 || state.compare("PLCR") == 0) {
+        trajectory = prep_lane_change_trajectory(state, predictions);
+    }
+    return trajectory;
+}
+
+
+vector<float> Vehicle::get_kinematics(map<int, vector<Vehicle>> predictions, int lane) {
+    /*
+    Gets next timestep kinematics (position, velocity, acceleration)
+    for a given lane. Tries to choose the maximum velocity and acceleration,
+    given other vehicle positions and accel/velocity constraints.
+    */
+    float max_velocity_accel_limit = this->max_acceleration + this->v;
+    float new_position;
+    float new_velocity;
+    float new_accel;
+    Vehicle vehicle_ahead;
+    Vehicle vehicle_behind;
+
+    if (get_vehicle_ahead(predictions, lane, vehicle_ahead)) {
+
+        if (get_vehicle_behind(predictions, lane, vehicle_behind)) {
+            new_velocity = vehicle_ahead.v; //must travel at the speed of traffic, regardless of preferred buffer
+        } else {
+            float max_velocity_in_front = (vehicle_ahead.s - this->s - this->preferred_buffer) + vehicle_ahead.v - 0.5 * (this->a);
+            new_velocity = min(min(max_velocity_in_front, max_velocity_accel_limit), this->target_speed);
+        }
+    } else {
+        new_velocity = min(max_velocity_accel_limit, this->target_speed);
+    }
+
+    new_accel = new_velocity - this->v; //Equation: (v_1 - v_0)/t = acceleration
+    new_position = this->s + new_velocity + new_accel / 2.0;
+    return{new_position, new_velocity, new_accel};
+
+}
